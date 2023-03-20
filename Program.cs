4123e000b07772775df2b96ed5b8dfc964e58bfc
Kroshmal1 <127083651+Kroshmal1@users.noreply.github.com>
2023-03-20 08:59:58 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] masses = { 68.4, 23.5, 71.8, 29.1, 212.3 };
+            double totalMass = 0;
+            foreach (double mass in masses) 
+            {
+                totalMass += mass;
+            }
+            Console.WriteLine($"Общая масса груза: {totalMass} кг");
+        }
+    }
+}
